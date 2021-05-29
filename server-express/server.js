b64c2bc7c08ab86fc23f99b678f1f2d9f8c47874
Lachlan Heywood <lachlan@heywood.net.au>
2021-05-28 21:22:11 -0400

    add express et. al. and set up app structure

--- server-express/server.js
+++ server-express/server.js
@@ -1,1 +1,28 @@
-process.exit(1);
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const handleErrors = require('./middleware/handleErrors');
+
+const indexRouter = require('./routes/index');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+app.use(handleErrors);
+
+module.exports = app;
