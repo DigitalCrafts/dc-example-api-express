7a9d18cdb95cc2528b00e3d6c8db8ee9311303a3
Lachlan Heywood <lachlan@heywood.net.au>
2021-05-28 21:25:58 -0400

    comments

--- server-express/server.js
+++ server-express/server.js
@@ -3,26 +3,28 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
 const handleErrors = require('./middleware/handleErrors');
 
 const indexRouter = require('./routes/index');
 
+// Set up Express Application
 const app = express();
 
+// Middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Routes
 app.use('/', indexRouter);
 
-// catch 404 and forward to error handler
+// Error Handling
 app.use((req, res, next) => {
   next(createError(404));
 });
-
 app.use(handleErrors);
 
+// Export for use in ./bin/www
 module.exports = app;
