21fbe6e522fbb4aa5eca1cd5c31b5792911d3072
Lachlan Heywood <lachlan@heywood.net.au>
2021-05-29 21:49:52 -0400

    add first route and swagger config

---
+++ server-express/lib/swagger.js
@@ -0,0 +1,43 @@
+const expressJSDocSwagger = require('express-jsdoc-swagger');
+
+/**
+ * Configure given application to use look for JSDoc comments and configure the Swagger Specification Docs
+ * @param {import('express').Application} app
+ */
+function configureSwagger(app) {
+  // Swagger set up
+  /**
+   * @type {import('express-jsdoc-swagger').Options}
+   */
+  const options = {
+    openapi: '3.0.0',
+    info: {
+      title: 'DigitalCrafts Shop API',
+      version: '1.0.0',
+      contact: {
+        name: 'DigitalCrafts',
+        url: 'https://digitalcrafts.com',
+      },
+    },
+    basePath: '/api/v1',
+    servers: [
+      {
+        description: 'Development',
+        url: `http://localhost:${process.env.PORT || 3000}`,
+      },
+      {
+        description: 'Production',
+        url: `https://localhost:${process.env.PORT || 3000}`,
+      },
+    ],
+    baseDir: __dirname,
+    filesPattern: ['../routes/api/**/*.js', '../models/**/*.js'],
+    swaggerUIPath: '/docs',
+    exposeApiDocs: true,
+    apiDocsPath: '/docs.json',
+  };
+
+  expressJSDocSwagger(app)(options);
+}
+
+module.exports = configureSwagger;
