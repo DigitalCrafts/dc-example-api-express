5b634567be0f5cbe4402ed14cd5b68c8640f2523
Lachlan Heywood <lachlan@heywood.net.au>
2021-05-31 19:32:19 -0400

    only show published products

--- server-express/controllers/products/getProducts.js
+++ server-express/controllers/products/getProducts.js
@@ -1,7 +1,21 @@
+const { Op, DataTypes } = require('sequelize');
 const db = require('../../models');
 
+/**
+ * Get All published products
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
+ */
 async function getProducts(req, res) {
-  res.json(await db.Product.findAll());
+  res.json(
+    await db.Product.findAll({
+      where: {
+        publishedAt: {
+          [Op.lt]: Date.now(),
+        },
+      },
+    })
+  );
 }
 
 module.exports = getProducts;
